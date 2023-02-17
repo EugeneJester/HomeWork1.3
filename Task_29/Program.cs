67fe2e243d62843fdb6cbe06c2ad4699ff0f714f
Veronika <EugeneJester@mail.ru>
2023-02-18 01:33:19 +1200

    .\Task_29\

---
+++ Task_29/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 29: https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=5&id_topic=114&id_problem=699
+Console.Clear();
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+  array[i] = new Random().Next(150, 181); // [150, 180]
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+int myRost = Convert.ToInt32(Console.ReadLine()), count = 1;
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i] >= myRost)
+    count++; // count = count + 1;
+}
+Console.WriteLine(count);
